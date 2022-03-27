d3c9bab0263238ab85172c15e5433230d8de56e4
Michael Berkowitz <michael.berkowitz@gmail.com>
2022-03-27 19:46:04 -0400

    Add function to lower volume of WAV file

--- lib/surfex.ex
+++ lib/surfex.ex
@@ -1,2 +1,21 @@
 defmodule Surfex do
+  alias Surfex.WavFile
+  alias Surfex.WavFile.AudioData.Parsing
+
+  def lower_volume(infile, outfile, percentage \\ 0.5) do
+    wav = WavFile.read(infile)
+
+    channels = Parsing.split_audio_data_into_channels(wav)
+
+    channels =
+      Enum.map(channels, fn channel ->
+        Enum.map(channel, fn i -> round(i * percentage) end)
+      end)
+
+    audio_data = Parsing.restore_audio_data_from_channels(channels, wav.bits_per_sample)
+
+    new_wav = %{wav | data: audio_data}
+
+    WavFile.write(new_wav, outfile)
+  end
 end
