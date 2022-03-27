bf1cf1b204c8e4ade27cca1c173dba6aa29e79f7
Michael Berkowitz <michael.berkowitz@gmail.com>
2022-03-26 22:34:03 -0400

    mix new

---
+++ lib/surfex.ex
@@ -0,0 +1,18 @@
+defmodule Surfex do
+  @moduledoc """
+  Documentation for `Surfex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Surfex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/surfex_test.exs
@@ -0,0 +1,8 @@
+defmodule SurfexTest do
+  use ExUnit.Case
+  doctest Surfex
+
+  test "greets the world" do
+    assert Surfex.hello() == :world
+  end
+end
